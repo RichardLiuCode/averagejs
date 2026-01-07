21a1b8c66a59cfc2176fc296d6295cb1ea855b14
Richard Liu <richardadesigner@gmail.com>
2026-01-06 19:21:58 -0800

    update

--- script.js
+++ script.js
@@ -42,5 +42,25 @@ function getMedian(inputList) {
 }
 
 function getMode(inputList) {
+    let inputNumbers = inputList;
+    let numbersFromGreatestToLeast = [];
+    while (inputNumbers.length > 0) {
+        let largest = inputNumbers[0];
+        for (let i = 0; i < inputNumbers.length; i++) {
+            if (isNaN(inputNumbers[i]) || (inputNumbers[i] == "") || (!inputNumbers[i])) {
+                throw new Error("There is an item in the input list which is not a number, all the items in the input list are required to be a number");
+            }
+            if (inputNumbers[i] > largest) {
+                largest = inputNumbers[i];
+            }
+        }
+        for (let i = 0; i < inputNumbers.length; i++) {
+            if (inputNumbers[i] == largest) {
+                numbersFromGreatestToLeast.push(largest);
+                inputNumbers.splice(i, 1);
+            }
+        }
 
+    }
+    let arrangedNumbers = numbersFromGreatestToLeast.reverse();
 }
